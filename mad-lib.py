cfcfc3249a581711db8561249e4febfd242463c2
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-02-12 12:43:12 -0800

    :)

---
+++ mad-lib.py
@@ -0,0 +1,9 @@
+ad1 = input('adjective ')
+noun1 = input('noun ')
+noun2 = input('noun ')
+ad2 = input('adjective ')
+
+print('The ' + ad1)
+print(noun1 + ' was')
+print(noun2 + ' and ' + ad2)
+print(' all the way home')
